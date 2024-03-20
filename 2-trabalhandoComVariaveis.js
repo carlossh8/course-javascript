03e7c09fa17bb349ec068b6c2dcab0987cfc5733
carlossh8 <carlos.sh8@outlook.com>
2024-03-20 11:25:52 -0300

    Aula Módulo 2 - Trabalhando Com Variáveis

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 24;
+const nome = "Carlos";
+
+//não fazer
+//mes = "Janeiro";
